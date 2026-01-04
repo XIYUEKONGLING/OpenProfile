a0b51d4fc6ba51e425598ef84138b34c6ca3d996
XIYUEKONGLING <229533969+XIYUEKONGLING@users.noreply.github.com>
2026-01-05 04:53:20 +0800

    refactor: make verification code optional in AddEmailRequestDto

--- OpenProfileServer/Models/DTOs/Account/AddEmailRequestDto.cs
+++ OpenProfileServer/Models/DTOs/Account/AddEmailRequestDto.cs
@@ -8,6 +8,6 @@ public class AddEmailRequestDto
     [EmailAddress]
     public string Email { get; set; } = string.Empty;
     
-    [Required]
-    public string Code { get; set; } = string.Empty;
+    // [Required]
+    public string? Code { get; set; } = string.Empty;
 }
